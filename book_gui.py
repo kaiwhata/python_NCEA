70322ef794eaad04976e088a0660fec476eb33c5
Elf Eldridge <kaiwhata@gmail.com>
2014-05-01 15:58:53 +1200

    Update book_gui.py

--- book_gui.py
+++ book_gui.py
@@ -38,15 +38,18 @@ class GUI:
         # similar to root in other texts
         window = Tk()
         window.title("Data Entry Screen")
+        #setting root window and size
+        window.minsize(width=400, height=200)
 
+        #INITIALIZATION VARIABLES
+        #this variable stores whether the data has been validated or not
+        self.ready_to_write = False
+        #this will contain the list of all books entered via the gui
+        self.recordlist = []
 
-        self.canvas = Canvas(window, height = 10, width=400, bg='blue')
-        self.canvas.create_rectangle(10, 20, 30, 10, fill='white')
-        self.canvas.pack()
-
-
+        #creating label and field variable in GUI for each entry field
         name_label = Label(window, text='Enter Title:')
-        name_label.pack()
+        name_label.pack() #.pack() places the component in the window
         self.name_field = Entry(window)
         self.name_field.pack()
 
@@ -60,7 +63,8 @@ class GUI:
         self.isbn_field = Entry(window)
         self.isbn_field.pack()
 
-
+        #creates a button. The command function is run when the button is pressed
+        #the 'command=self.doSubmit' is an example of a callback method
         button_label = Label(window, text='Press to validate:')
         button = Button(window, text='Submit', command=self.doSubmit)
 
@@ -71,17 +75,12 @@ class GUI:
         button_label1.pack()
         button1.pack()
 
-
-        error_label = Label(window, text='')
-        error_label.pack()
-
-        self.ready_to_write = False
-        self.recordlist = []
+        #waiting for user input - event driven program
         window.mainloop()
 
 
     def doSubmit(self):
-
+        #this is the callback method for the 'Submit' button
         if len(self.name_field.get()) <1 or len(self.author_field.get()) <1 or len(self.isbn_field.get()) <1:
             tkinter.messagebox.showwarning('Warning!','Please enter a value for all fields')
         else:
@@ -100,21 +99,24 @@ class GUI:
 
 
     def writetocsv(self):
+        #this is the callback method for the 'write to csv' button
         import csv
         file_name = 'database.txt'
-        if self.ready_to_write:
-            ofile = open(file_name, 'a') #open with write privelages
+
+        if self.ready_to_write: #cheacks data has been previously validated
+            ofile = open(file_name, 'a') #open with write('w') or append('a') privelages
             writer = csv.writer(ofile, delimiter=',')
+            #cycles through list of records created by gui
             for record in self.recordlist:
                 print(record.get_title())
                 writer.writerow([record.get_title(),record.get_author(), record.get_isbn(), record.get_is_read()])
+            #explicitly closes the output file
             ofile.close()
         else:
-            txt_var = StringVar()
-            label_object = Label(window, textvariable=txt_var)
-            label_object.pack()
-            txt_var.set('Error, you need to Validate your data')
+            tkinter.messagebox.showwarning('Error!', 'You need to Validate your data')
+
+        self.ready_to_write= False
         tkinter.messagebox.showinfo('Notice',file_name+' File Generated Sucessfully')
 
-#setupGUI()
+#initialises the programme
 GUI()
